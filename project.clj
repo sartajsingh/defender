9d219aedd43c2186bbc16b2d8514566a53e5a81b
Sartaj Singh <srtaj.sngh@gmail.com>
2018-03-12 23:52:43 +0530

    Add hystrix support methods

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject defender "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The Defender"
+  :url "https://github.com/sartajsingh/defender.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.netflix.hystrix/hystrix-clj "1.5.13"]
+                 [com.netflix.hystrix/hystrix-codahale-metrics-publisher "1.5.13"
+                  :exclusions [io.dropwizard.metrics/metrics-core]]
+                 [com.netflix.hystrix/hystrix-metrics-event-stream "1.5.13"]])
--- src/defender/core.clj
+++ src/defender/core.clj
@@ -1,6 +1,53 @@
-(ns defender.core)
+(ns defender.core
+  (:require [com.netflix.hystrix.core :as hystrix]
+            [clojure.tools.logging :as log])
+  (:import (com.netflix.hystrix HystrixCommand HystrixCommand$Setter
+                                HystrixCommandProperties HystrixThreadPoolProperties
+                                HystrixExecutable)
+           (com.netflix hystrix.strategy HystrixPlugins)
+           (com.netflix.hystrix.contrib.codahalemetricspublisher HystrixCodaHaleMetricsPublisher)
+           (com.codahale.metrics MetricRegistry)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn init-fn [{:keys [thread-count
+                       breaker-sleep-window-ms
+                       breaker-error-threshold-percentage
+                       execution-timeout-ms]}]
+  (fn [_ ^HystrixCommand$Setter setter]
+    (-> setter
+        (.andCommandPropertiesDefaults
+          (-> (HystrixCommandProperties/Setter)
+              (.withCircuitBreakerSleepWindowInMilliseconds breaker-sleep-window-ms)
+              (.withCircuitBreakerErrorThresholdPercentage breaker-error-threshold-percentage)
+              (.withExecutionTimeoutInMilliseconds execution-timeout-ms)))
+        (.andThreadPoolPropertiesDefaults
+          (-> (HystrixThreadPoolProperties/Setter)
+              (.withCoreSize thread-count))))))
+
+(defn- fallback-wrapper [fallback-fn]
+  (capture-logging-context
+    (fn []
+      (let [^HystrixCommand command hystrix/*command*]
+        (fallback-fn {:response-timed-out?         (.isResponseTimedOut command)
+                      :failed-execution-exeception (.getFailedExecutionException command)
+                      :circuit-breaker-open?       (.isCircuitBreakerOpen command)})))))
+
+(defn hystrix-conf [{:keys [config group-key thread-pool-key command-key run-fn fallback-fn]}]
+  (cond-> {:type            :command
+           :group-key       (hystrix/group-key group-key)
+           :command-key     (hystrix/command-key command-key)
+           :thread-pool-key (hystrix/thread-pool-key (or thread-pool-key group-key))
+           :init-fn         (init-fn config)
+           :run-fn          (capture-logging-context run-fn)}
+
+    (some? fallback-fn) (assoc :fallback-fn (fallback-wrapper fallback-fn))))
+
+(defn execute-with-hystrix [key-map]
+  (let [definition (hystrix-conf key-map)]
+    (.execute ^HystrixExecutable (hystrix/instantiate* definition))))
+
+(defmacro with-hystrix [key-map & body]
+  `(execute-with-hystrix (assoc ~key-map :run-fn (fn [] ~@body))))
+
+(defn register-metrics-publisher [^MetricRegistry metrics-registry]
+  (.registerMetricsPublisher (HystrixPlugins/getInstance) (HystrixCodaHaleMetricsPublisher. metrics-registry))
+  (log/info "Registered hystrix metrics publisher"))
